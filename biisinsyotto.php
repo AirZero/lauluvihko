4e4478a21729b1c7e197c4137ed0fd92d971480d
elventhor <elventhor@users.noreply.github.com>
2014-11-10 19:50:31 +0100

    Muuttujien nimet, input sanitointi.

--- biisinsyotto.php
+++ biisinsyotto.php
@@ -1,7 +1,7 @@
 <?php
-if(isset($_POST['field1']) && isset($_POST['field2'])) {
-    $data = '\chapter{'. $_POST['field1'] .'}' . "\n" . $_POST['field2'];
-    $tiedostonimi = 'biisit/' . $_POST['field1'] . '.txt' ;
+if(isset($_POST['laulun_nimi']) && isset($_POST['laulun_sanat'])) {
+    $data = '\chapter{'. $_POST['laulun_nimi'] .'}' . "\n" . $_POST['laulun_sanat']; // TODO: Sanitoi input!
+    $tiedostonimi = 'biisit/' . preg_replace('/[^A-Za-z0-9_\-]/', '_', $_POST['field1']) . '.txt' ; // Ei tyhmää paskaa tiedostonimeen
 //    $biisitiedosto = fopen(tmp/ $tiedostonimi, "w");
     $ret = file_put_contents($tiedostonimi /*'tmp/mydata.txt'/*$_POST['field1']*/, $data, FILE_APPEND | LOCK_EX);
     if($ret === false) {
